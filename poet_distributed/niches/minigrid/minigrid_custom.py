e6e54c73fc660fcbde3c6ca58196e2a62bc92b72
gdicker1 <43178166+gdicker1@users.noreply.github.com>
2020-12-06 17:01:55 -0700

    Create minigrid_custom with only Env_config so far
    This Env_config describes the parameters used to define the genomes of a minigrid environment.

---
+++ poet_distributed/niches/minigrid/minigrid_custom.py
@@ -0,0 +1,11 @@
+from collections import namedtuple
+
+Env_config = namedtuple('Env_config', [
+    'name',
+    'lava prob',
+    'obstacle lvl',
+    'box to ball prob',
+    'door prob',
+    'wall prob',
+])
+
